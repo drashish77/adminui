1ad255776fbcc2bf9c3fa5178cbb237794bac195
ASHISH <dr.ashish.g77@gmail.com>
2021-11-10 01:23:40 +0530

    Created Ui of Edit form

--- src/components/Search/EditForm.js
+++ src/components/Search/EditForm.js
@@ -1,28 +1,35 @@
 const EditForm = ({ id, name, email, role, onSubmit, handleChange }) => {
   return (
     <form className='edit_form' key={id} onSubmit={(e) => onSubmit(e, id)}>
-      <input
-        type='text'
-        name='name'
-        placeholder='enter Name'
-        value={name}
-        onChange={handleChange}
-      />
-      <input
-        type='email'
-        name='email'
-        placeholder='enter email'
-        value={email}
-        onChange={handleChange}
-      />
-      <input
-        type='text'
-        name='role'
-        placeholder='enter role'
-        value={role}
-        onChange={handleChange}
-      />
-      <button type='submit'>Update</button>
+      <div className='edit_form_name_email'>
+        <input
+          type='text'
+          name='name'
+          placeholder='enter Name'
+          value={name}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type='email'
+          name='email'
+          placeholder='enter email'
+          value={email}
+          onChange={handleChange}
+          required
+        />
+
+        <input
+          type='text'
+          name='role'
+          placeholder='enter role'
+          value={role}
+          onChange={handleChange}
+          required
+        />
+
+        <button type='submit'>Update</button>
+      </div>
     </form>
   )
 }
